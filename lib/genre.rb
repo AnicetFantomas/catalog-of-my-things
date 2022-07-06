e11f51c5600ead8507d4eb004f7609204f46d29c
BisiAlfred <kalumunabisimwa5@gmail.com>
2022-07-06 14:18:50 +0200

    Create Genre class with an association to the Item class

---
+++ lib/genre.rb
@@ -0,0 +1,14 @@
+class Genre
+  attr_accessor :name
+  attr_reader :items
+
+  def initialize(name)
+    @name = name
+    @items = []
+  end
+
+  def add_item(item)
+    @items << item
+    item.genre = self
+  end
+end
