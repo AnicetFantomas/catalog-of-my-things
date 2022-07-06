d9e131f621cbe40bfee27a36e80f0214cdde3d21
AnicetFantomas <94958024+AnicetFantomas@users.noreply.github.com>
2022-07-06 13:34:15 +0200

    Update files

--- author.rb
+++ author.rb
@@ -1,2 +1,9 @@
 class Author
+    def initialize(first_name, last_name, items)
+        @id = Random.rand(1..1000)
+        @first_name = first_name
+        @last_name = last_name
+        items = []
+    end
+
 end
--- game.rb
+++ game.rb
@@ -1,5 +1,5 @@
 class Game 
-    initialize (multiplayer, last_played_at)
+    def initialize (multiplayer, last_played_at)
         @multiplayer = multiplayer
         @last_played_at = last_played_at
     end
