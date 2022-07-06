fa0d9a1a19f2c62c88ebe3c7827f86b6a259d365
AnicetFantomas <94958024+AnicetFantomas@users.noreply.github.com>
2022-07-06 15:22:33 +0200

    Update game and author files

--- author.rb
+++ author.rb
@@ -1,11 +1,17 @@
+require './item'
+
 class Author
-    def initialize(first_name, last_name, items)
+    attr_reader :items
+    attr_accessor :first_name, :last_name
+
+    def initialize(first_name, last_name)
         @id = Random.rand(1..1000)
         @first_name = first_name
         @last_name = last_name
         items = []
     end
 
-    def add_item
+    def add_item(item)
+        @items = item 
     end
 end
--- game.rb
+++ game.rb
@@ -1,9 +1,12 @@
-class Game 
+require './item'
+
+class Game < Item
     def initialize (multiplayer, last_played_at)
         @multiplayer = multiplayer
         @last_played_at = last_played_at
     end
 
     def can_be_achieved? 
+        
     end
 end
